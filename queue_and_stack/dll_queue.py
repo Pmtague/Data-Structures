471799f46d1814bc3392f177b8027f3e7f145d56
Penny Tague <pmtague9@gmail.com>
2020-01-29 16:49:40 -0800

    Finished day 1 of data structures, queues and stacks

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,27 +5,35 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
+
         # Declare class properties
         self.size = 0
+
         # Store the DoublyLinkedList class as a property
         self.storage = DoublyLinkedList()
 
     # Add a node to the back of the queue (the head)
     def enqueue(self, value):
-		# Increment the size of the queue
+
+        # Increment the size of the queue
         self.size += 1
-		# Create a new node and add it to the head of the DLL (The end of the queue)
+
+        # Create a new node and add it to the head of the DLL (The end of the queue)
         self.storage.add_to_head(value)
 
-	# Remove a node from the front of the queue(the tail)
+    # Remove a node from the front of the queue(the tail)
     def dequeue(self):
-		# If the queue is not empty, deincrement the size and remove the tail of the DLL (The beginning of the queue)
-        if self.size > 0:
+
+        # If the queue is not empty, deincrement the size and remove the tail of the DLL (The beginning of the queue)
+        if self.len() > 0:
             self.size -= 1
-            self.storage.remove_from_tail()
-		# If the queue is empty, return none
+            value = self.storage.remove_from_tail()
+            return value
+
+        # If the queue is empty, return none
         else:
             return None
-	# Return the size of the queue
+			
+    # Return the size of the queue
     def len(self):
         return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,44 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# Stacks are LIFO
 class Stack:
     def __init__(self):
+
+        # Declare stack props
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
 
+        # Why is our DLL a good choice to store our elements? 
+        self.storage = DoublyLinkedList()
+
+    # Add a node to the top of the stack (the head)
     def push(self, value):
-        pass
 
+        # Increment stack size
+        self.size += 1
+
+        # Create the new node and add it to the top
+        self.storage.add_to_head(value)
+
+    # Remove a node from the top of the stack (the head)
     def pop(self):
-        pass
 
+        # If the stack is not empty, store the value from the current node, delete the node, and return the value
+        if self.len() > 0:
+
+            # Store the value of the node we want to delete and delete it
+            value = self.storage.remove_from_head()
+
+            # Decrement the stack size
+            self.size -= 1
+
+            # Return the deleted node's value
+            return value
+		
+        # If stack is empty, return none
+        else:
+            return None
+    
+    # Return the size of the stack
     def len(self):
-        pass
+        return self.size
